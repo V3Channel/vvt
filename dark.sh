478714006c08cb735bb4e1158f26f474f4fe476c
V3Channel <120969677+V3Channel@users.noreply.github.com>
2022-12-19 20:58:40 +0800

    Add files via upload

---
+++ dark.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear
+green='\e[0;32m'
+purple='\e[1;35m'
+NC='\e[0m'
+
+
+echo -e "                                                   " | lolcat
+echo -e "██████╗░░█████╗░██████╗░██╗░░██╗  ██╗░░░██╗██████╗░" | lolcat
+echo -e "██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ██║░░░██║╚════██╗" | lolcat
+echo -e "██║░░██║███████║██████╔╝█████═╝░  ╚██╗░██╔╝░█████╔╝" | lolcat
+echo -e "██║░░██║██╔══██║██╔══██╗██╔═██╗░  ░╚████╔╝░░╚═══██╗" | lolcat
+echo -e "██████╔╝██║░░██║██║░░██║██║░╚██╗  ░░╚██╔╝░░██████╔╝" | lolcat
+echo -e "╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ░░░╚═╝░░░╚═════╝░" | lolcat
+echo -e  ""
+echo -e  "${purple}            Menegur jangan sampai menghina,"
+echo -e  "${purple}            Mendidik jangan sampai memaki,"
+echo -e  "${purple}            Meminta jangan sampai memaksa,"
+echo -e  "${purple}           Memberi jangan sampai mengungkit,"
+echo -e  "${purple}     Disaat kamu menyia-nyiakan orang yang tulus,"
+echo -e  "${purple}       suatu saat kamu akan merasakan sakitnya," 
+echo -e  "${purple}     disia-siakan dengan orang yang kamu sayang."
+echo -e  ""
+echo -e  "${green}Type menu to create config"${NC}
+echo -e  ""
+
